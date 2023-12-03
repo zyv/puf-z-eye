33c9852f31fefcd4f75120c7a4784c096b4eab23
Yury V. Zaytsev <yury.zaytsev@moneymeets.com>
2023-12-03 11:27:13 +0100

    Fix bug in total blocking algorithm

--- src/content-script.js
+++ src/content-script.js
@@ -7,12 +7,16 @@ chrome.storage.sync.get(['authors', 'hideAnswers']).then(result => {
       element => element.innerText === author
     )
 
+    const elementsToRemove = new Set()
+
     for (const post of postsToHide) {
       const heading = post.closest('tr')
       const messages = heading.nextElementSibling
       const buttons = messages.nextElementSibling
-      buttons.remove()
-      messages.remove()
+      elementsToRemove.add(messages)
+      elementsToRemove.add(buttons)
     }
+
+    elementsToRemove.forEach(element => element.remove())
   }
 })
